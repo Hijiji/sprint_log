51117b7c8ee11ab80b35c9d6b857c4064c75142b
Hijiji <wlals1169@naver.com>
2025-12-29 23:15:04 +0900

    feat: 앱 초기화 및 보안 설정 통합 (Config, Swagger, helmet, cors, logger)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 import configuration from './config/configuration';
 import { validationSchema } from './config/validation';
 
@@ -20,6 +21,7 @@ import { validationSchema } from './config/validation';
         abortEarly: false,
       },
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
