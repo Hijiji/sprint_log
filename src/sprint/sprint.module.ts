743b0d9b45de720f7683eab24e4e151b659f2a60
Hijiji <wlals1169@naver.com>
2026-01-02 01:15:43 +0900

    feat : repository 구현-복잡한 orm 사용을 위함

--- src/sprint/sprint.module.ts
+++ src/sprint/sprint.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SprintService } from './sprint.service';
 import { SprintController } from './sprint.controller';
+import { Sprint } from 'src/database/entities/sprint.entity';
+import { Member } from 'src/database/entities/member.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [SprintController],
   providers: [SprintService],
+  imports: [TypeOrmModule.forFeature([Sprint, Member])],
 })
 export class SprintModule {}
--- src/sprint/sprint.repository.ts
+++ src/sprint/sprint.repository.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSprintDto } from './dto/create-sprint.dto';
-import { UpdateSprintDto } from './dto/update-sprint.dto';
 import { Sprint } from '../database/entities/sprint.entity';
-import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { Member } from 'src/database/entities/member.entity';
 
 @Injectable()
@@ -12,23 +10,4 @@ export class SprintRepository {
     @InjectRepository(Sprint) private sprint: Repository<Sprint>,
     @InjectRepository(Member) private member: Repository<Member>,
   ) {}
-  create(createSprintDto: CreateSprintDto) {
-    return 'This action adds a new sprint';
-  }
-
-  findAll() {
-    return `This action returns all sprint`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} sprint`;
-  }
-
-  update(id: number, updateSprintDto: UpdateSprintDto) {
-    return `This action updates a #${id} sprint`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} sprint`;
-  }
 }
