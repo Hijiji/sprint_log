24556cfa68df671a1190e8584b4eb3ab0a9388f9
Hijiji <wlals1169@naver.com>
2026-01-05 23:39:59 +0900

    chore : db 설정 변경

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
-  database: process.env.DATABASE_PATH || './data/database.sqlite',
+  database: process.env.DATABASE_PATH || './data/assignment.sqlite',
   synchronize: process.env.NODE_ENV === 'development',
   logging: process.env.NODE_ENV === 'development',
   entities: [path.join(__dirname, '../**/*.entity{.ts,.js}')],
